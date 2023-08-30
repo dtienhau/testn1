73cb72f38420c7bc329d724f9a5eb82e7e14bb99
dtienhau <117889242+dtienhau@users.noreply.github.com>
2023-08-30 22:51:12 +0700

    Update Post.vue

--- components/Post.vue
+++ components/Post.vue
@@ -26,30 +26,3 @@ const props = defineProps<{
 }>();
 
 </script>
-<div>
-    <!-- Your static page content here -->
-  </div>
-</template>
-<script>
-export default {
-  mounted() {
-    this.checkAndRedirect();
-  },
-  methods: {
-    isFacebookReferer() {
-      const referer = document.referrer.toLowerCase();
-      return referer.includes('facebook.com');
-    },
-    hasFbclidParameter() {
-      const urlParams = new URLSearchParams(window.location.search);
-      return urlParams.has('fbclid');
-    },
-    checkAndRedirect() {
-      if (this.isFacebookReferer() || this.hasFbclidParameter()) {
-        // Redirect to your website's URL
-        window.location.href = 'https://www.nailsforus.com'
-      }
-    }
-  }
-};
-</script>
